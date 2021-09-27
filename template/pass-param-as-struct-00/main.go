a4d6ac0e42d52d55e0484387c2e8614b51006326
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-09-27 10:58:38 +0800

    Template: Pass a simple struct

---
+++ template/pass-param-as-struct-00/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+type sage struct {
+	Name  string
+	Motto string
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("template.gohtml"))
+}
+
+func main() {
+	buddha := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
+	}
+
+	err := tpl.Execute(os.Stdout, buddha)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
